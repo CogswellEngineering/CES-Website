f24002a6813f7d612ff80505ac34b95022c0752f
Prince Christian Basiga <princebasiga@gmail.com>
2018-06-16 03:32:07 -0700

    Updated StyledForm to all be on one page instead of separate

--- app/components/StyledForm/index.js
+++ app/components/StyledForm/index.js
@@ -3,5 +3,34 @@ import styled from 'styled-components';
 
 export default styled.form`
 
+    border: 2px solid black;
+
+`;
+
+export const StyledButton = styled.button`
+
+    border: 1px solid black;
+
+`;
+
+
+
+export const StyledLabel = styled.label`
+
+    color:green;
+
+`;
+
+
+export const ErrorMessage =  styled.p`
+
+    color:red;
+    text-weight:bold;
+
+`;
+
+export const StyledInput =  styled.input`
+
+    border:2px solid black;
 
 `;
